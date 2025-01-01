76c2fd647d3b0597c38cd38aaedabc0e6240ac08
vestr-at-work <petera-david@seznam.cz>
2025-01-01 20:12:56 +0100

    Add basic text and styling

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+var popup = L.popup();
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
 
 var map = L.map('map').setView([50.079420697915396, 14.451504480493412], 13);
 
@@ -5,3 +14,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([50.079420697915396, 14.451504480493412]).addTo(map);
+marker.bindPopup("<b>UUUU!</b><br>Ginkgo here!.").openPopup();
+
+map.on('click', onMapClick);
