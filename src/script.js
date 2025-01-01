f4720a3d09ee7097b6c5baf234868c6ee6e6375e
vestr-at-work <petera-david@seznam.cz>
2025-01-01 19:23:02 +0100

    Initial setup

---
+++ src/script.js
@@ -0,0 +1,7 @@
+
+var map = L.map('map').setView([50.079420697915396, 14.451504480493412], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
