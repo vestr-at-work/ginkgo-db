7e28c52ece2bc3096b99504fb962dc209f2a0f37
vestr-at-work <petera-david@seznam.cz>
2025-01-01 23:42:36 +0100

    Add basic form for adding new items

--- src/script.js
+++ src/script.js
@@ -1,21 +1,81 @@
 var popup = L.popup();
+let geoJsonData = {};
+var map = L.map('map').setView([50.079420697915396, 14.451504480493412], 13);
+var dialog = document.getElementById("addGinkgoDialog");
 
+function addNewGinkgo(lat, lng) {
+    dialog.style.display = "block";
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+    console.log("add new ginkgo" + lat.toString() + lng.toString());
 }
 
-var map = L.map('map').setView([50.079420697915396, 14.451504480493412], 13);
+window.onclick = function(event) {
+    if (event.target == dialog) {
+        dialog.style.display = "none";
+    }
+} 
+
+function onMapClick(e) {
+    let popupContent = `<h3>Do you want to add new Ginkgo?<h3>
+                        <input type="button" id="addNewButton" onclick="addNewGinkgo(${e.latlng.lat}, ${e.latlng.lng})" value="Yes"/>`;
+    popup
+        .setLatLng(e.latlng)
+        .setContent(popupContent)
+        .openOn(map);
+
+    // const newFeature = {
+    //     "type": "Feature",
+    //     "geometry": {
+    //         "type": "Point",
+    //         "coordinates": [e.latlng.lng, e.latlng.lat]
+    //     },
+    //     "properties": {
+    //         "title": "New Tree",
+    //         "description": "A newly added tree."
+    //     }
+    // };
+
+    // // Add the new feature to the GeoJSON data
+    // geoJsonData.features.push(newFeature);
+
+    // console.log(geoJsonData);
+    // // this is ugly CHANGE it to be more efficient!!!
+    // addDataToMap(geoJsonData, map);
+    
+}
+
+function addDataToMap(data, map) {
+    L.geoJSON(data, {
+        onEachFeature: (feature, layer) => {
+            let popupContent = `<h3>${feature.properties.title}</h3>
+                                <p>${feature.properties.description}</p>`;
+            if (feature.properties.image) {
+                popupContent += `<img src="${feature.properties.image}" alt="${feature.properties.title}" style="width:100px;">`;
+            }
+            layer.bindPopup(popupContent);
+        }
+    }).addTo(map);
+}
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([50.079420697915396, 14.451504480493412]).addTo(map);
-marker.bindPopup("<b>UUUU!</b><br>Ginkgo here!.").openPopup();
+// Fetch GeoJSON data from the file
+fetch('data.geojson')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        geoJsonData = data;
+        addDataToMap(geoJsonData, map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON:', error);
+    });
 
 map.on('click', onMapClick);
